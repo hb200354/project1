3a0a75e22697a9638a65fa03bc93ab1379cba383
amino6413-lab <amino6413@gmail.com>
2025-11-06 00:04:15 +0900

    Last?

---
+++ db/detail.sql
@@ -0,0 +1,36 @@
+CREATE TABLE product_detail_spec (
+  name          VARCHAR(100),
+  wood_frame    VARCHAR(100),
+  wood_panel    VARCHAR(100),
+  suggestion    SMALLINT
+)
+
+
+
+INSERT INTO product_detail_spec (name, wood_frame, wood_panel, suggestion)
+VALUES ('아루니 범퍼침대 SS 4면형', '박스 프레임', '깔판', 12);
+
+INSERT INTO product_detail_spec (name, wood_frame, wood_panel, suggestion)
+VALUES ('아루니 범퍼침대 패밀리형 (SS+Q)', '박스 프레임', '깔판', 36);
+
+INSERT INTO product_detail_spec (name, wood_frame, wood_panel, suggestion)
+VALUES ('마론 유아 교구장', '박스 프레임', '깔판', 36);
+
+INSERT INTO product_detail_spec (name, wood_frame, wood_panel, suggestion)
+VALUES ('마론 2in1 장난감정리함', '박스 프레임', '깔판', 36);
+
+INSERT INTO product_detail_spec (name, wood_frame, wood_panel, suggestion)
+VALUES ('마론 유아 책상의자 세트', '박스 프레임', '깔판', 12);
+
+
+INSERT INTO product_detail_spec (name, wood_frame, wood_panel, suggestion)
+VALUES ('마론 유아 의자', '박스 프레임', '깔판', 36);
+
+
+INSERT INTO product_detail_spec (name, wood_frame, wood_panel, suggestion)
+VALUES ('마론 유아 책상', '박스 프레임', '깔판', 36);
+
+INSERT INTO product_detail_spec (name, wood_frame, wood_panel, suggestion)
+VALUES ('마론 어린이 책상', '박스 프레임', '깔판', 36);
+
+
--- db/seed.sql
+++ db/seed.sql
@@ -4,11 +4,13 @@ insert into products(
     image_url,
     hover_image_url,
     category
-) values ( '포미오 가드침대 스윙도어형+범퍼 SET', '2,893,000', '../images/스토리침대.png', '../images/베스트이미지1.jpg', 'best'),
+) values 
+( '포미오 가드침대 스윙도어형+범퍼 SET', '2,893,000', '../images/스토리침대.png', '../images/베스트이미지1.jpg', 'best'),
 ('포미오 가드침대 스윙도어형', '2,150,000', '../images/스토리침대.png', '../images/베스트이미지2.jpg', 'best'),
 ('아루니 범퍼침대 SS 4면형', '2,450,000',  '../images/침대.png', '../images/침대1.jpg', 'best'),
 ('아루니 범퍼침대 패밀리형 (SS+Q)', '3,090,000', '../images/패밀리.png', '../images/패밀리2.jpg', 'best'),
 ('마론 유아 교구장', '487,000',  '../images/교구장.png', '../images/소가구교구장.jpg', 'storage'),
-('마론 2in1 장난감정리함', '380,000',  '../images/교구장.png', '../images/소가구교구장.jpg', 'storage'),
+('마론 2in1 장난감정리함', '380,000',  '../images/교구장.png', '../images/소가구교구장.jpg', 'storage')
 
 
+'../images/소가구교구장.jpg,../''images/소가구교구장.jpg''../images/침대1.jpg'
