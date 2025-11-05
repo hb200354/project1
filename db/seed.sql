2525cedc55e2d7cfdb71df1d2c142ff1f09feade
tnwls00120012-debug <tnwls00120012@gmail.com>
2025-11-05 17:18:10 +0900

    테이블

--- db/seed.sql
+++ db/seed.sql
@@ -1,35 +1,14 @@
--- 예시
--- insert into users (
---    username,
---    password,
---    name,
---    email
--- ) values ( 'student1',
---            '1111',
---            '김학생',
---            'student1@example.com' ),( 'dev123',
---                                       'abcd',
---                                       '이개발',
---                                       'dev123@example.com' ),( 'admin',
---                                                                '0000',
---                                                                '관리자',
---                                                                'admin@example.com' );
+insert into products(
+    name,
+    price,
+    image_url,
+    hover_image_url,
+    category
+) values ( '포미오 가드침대 스윙도어형+범퍼 SET', '2,893,000', '../images/스토리침대.png', '../images/베스트이미지1.jpg', 'best'),
+('포미오 가드침대 스윙도어형', '2,150,000', '../images/스토리침대.png', '../images/베스트이미지2.jpg', 'best'),
+('아루니 범퍼침대 SS 4면형', '2,450,000',  '../images/침대.png', '../images/침대1.jpg', 'best'),
+('아루니 범퍼침대 패밀리형 (SS+Q)', '3,090,000', '../images/패밀리.png', '../images/패밀리2.jpg', 'best'),
+('마론 유아 교구장', '487,000',  '../images/교구장.png', '../images/소가구교구장.jpg', 'storage'),
+('마론 2in1 장난감정리함', '380,000',  '../images/교구장.png', '../images/소가구교구장.jpg', 'storage'),
 
--- insert into products (
---    name,
---    price,
---    description,
---    image_url
--- ) values ( '기본 반팔 티셔츠',
---            15000,
---            '편하게 입을 수 있는 기본 반팔 티셔츠입니다.',
---            'https://via.placeholder.com/300x200?text=T-shirt' ),( '청바지',
---                                                                   32000,
---                                                                   '슬림핏 중청바지입니다.',
---                                                                   'https://via.placeholder.com/300x200?text=Jeans' ),( '블랙 자켓'
---                                                                   ,
---                                                                                                                        59000,
---                                                                                                                        '가을에 입기 좋은 블랙 자켓입니다.'
---                                                                                                                        ,
---                                                                                                                        'https://via.placeholder.com/300x200?text=Jacket'
---                                                                                                                        );
+
