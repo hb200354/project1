788042890c4f5fd7494d59879a6cd6aa90a0053b
Yebom <a80760191@gmail.com>
2025-11-06 08:50:35 +0900

    .

--- db/seed.sql
+++ db/seed.sql
@@ -28,4 +28,41 @@ insert into products (
                                                                      '380,000',
                                                                      '../images/교구장.png',
                                                                      '../images/소가구교구장.jpg',
-                                                                     'storage' );
+                                                                     'storage' ),( '마론 2in1 장난감정리함',
+                                                                                   '380,000',
+                                                                                   '../images/소가구정리함.png',
+                                                                                   '../images/소가구정리함.jpg',
+                                                                                   'storage' ),( '마론 유아 책상의자 세트',
+                                                                                                 '465,600',
+                                                                                                 '../images/책상의자세트.png',
+                                                                                                 '../images/소가구책상의자세트.jpg',
+                                                                                                 'desk_chair' ),( '마론 유아 의자',
+                                                                                                                  '150,000',
+                                                                                                                  '../images/유아의자.png'
+                                                                                                                  ,
+                                                                                                                  '../images/소가구유아의자.jpg'
+                                                                                                                  ,
+                                                                                                                  'desk_chair'
+                                                                                                                  ),( '마론 유아 책상'
+                                                                                                                  ,
+                                                                                                                               '330,000'
+                                                                                                                               ,
+                                                                                                                               '../images/유아책상.png'
+                                                                                                                               ,
+                                                                                                                               '../images/소가구유아책상.jpg'
+                                                                                                                               ,
+                                                                                                                               'desk_chair'
+                                                                                                                               )
+                                                                                                                               ,
+                                                                                                                               (
+                                                                                                                               '마론 어린이 책상'
+                                                                                                                               ,
+                                                                                                                                            '410,000'
+                                                                                                                                            ,
+                                                                                                                                            '../images/어린이책상png'
+                                                                                                                                            ,
+                                                                                                                                            '../images/소가구어린이책상.jpg'
+                                                                                                                                            ,
+                                                                                                                                            'desk_chair'
+                                                                                                                                            )
+                                                                                                                                            ;
