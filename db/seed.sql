0ddbce28128a6b88dd1a996be3a87fcda719e617
Yebom <92360412+hb200354@users.noreply.github.com>
2025-11-06 17:47:01 +0900

    /

--- db/seed.sql
+++ db/seed.sql
@@ -4,61 +4,136 @@ insert into products (
    image_url,
    hover_image_url,
    category
-) values ( '포미오 가드침대 스윙도어형+범퍼 SET',
-           '2,893,000',
-           '../images/스토리침대.png',
-           '../images/베스트이미지1.jpg',
-           'best' ),( '포미오 가드침대 스윙도어형',
-                      '2,150,000',
-                      '../images/스토리침대.png',
-                      '../images/베스트이미지2.jpg',
-                      'best' ),( '아루니 범퍼침대 SS 4면형',
-                                 '2,450,000',
-                                 '../images/침대.png',
-                                 '../images/침대1.jpg',
-                                 'best' ),( '아루니 범퍼침대 패밀리형 (SS+Q)',
-                                            '3,090,000',
-                                            '../images/패밀리.png',
-                                            '../images/패밀리2.jpg',
-                                            'best' ),( '마론 유아 교구장',
-                                                       '487,000',
-                                                       '../images/교구장.png',
-                                                       '../images/소가구교구장.jpg',
-                                                       'storage' ),( '마론 2in1 장난감정리함',
-                                                                                   '380,000',
-                                                                                   '../images/소가구정리함.png',
-                                                                                   '../images/소가구정리함.jpg',
-                                                                                   'storage' ),( '마론 유아 책상의자 세트',
-                                                                                                 '465,600',
-                                                                                                 '../images/책상의자세트.png',
-                                                                                                 '../images/소가구책상의자세트.jpg',
-                                                                                                 'desk_chair' ),( '마론 유아 의자',
-                                                                                                                  '150,000',
-                                                                                                                  '../images/유아의자.png'
-                                                                                                                  ,
-                                                                                                                  '../images/소가구유아의자.jpg'
-                                                                                                                  ,
-                                                                                                                  'desk_chair'
-                                                                                                                  ),( '마론 유아 책상'
-                                                                                                                  ,
-                                                                                                                               '330,000'
-                                                                                                                               ,
-                                                                                                                               '../images/유아책상.png'
-                                                                                                                               ,
-                                                                                                                               '../images/소가구유아책상.jpg'
-                                                                                                                               ,
-                                                                                                                               'desk_chair'
-                                                                                                                               )
-                                                                                                                               ,
-                                                                                                                               (
-                                                                                                                               '마론 어린이 책상'
-                                                                                                                               ,
-                                                                                                                                            '410,000'
-                                                                                                                                            ,
-                                                                                                                                            '../images/어린이책상png'
-                                                                                                                                            ,
-                                                                                                                                            '../images/소가구어린이책상.jpg'
-                                                                                                                                            ,
-                                                                                                                                            'desk_chair'
-                                                                                                                                            )
-                                                                                                                                            ;
+) values
+   ( '포미오 가드침대 스윙도어형+범퍼 SET',
+     2893000,
+     '../images/스토리침대.png',
+     '../images/베스트이미지1.jpg',
+     'best'
+   );
+
+insert into products1 (
+   name,
+   price,
+   image_url,
+   hover_image_url,
+   category
+) values
+   ( '포미오 가드침대 스윙도어형',
+     2150000,
+     '../images/스토리침대.png',
+     '../images/베스트이미지2.jpg',
+     'best'
+   );
+
+insert into products1 (
+   name,
+   price,
+   image_url,
+   hover_image_url,
+   category
+) values
+   ( '아루니 범퍼침대 SS 4면형',
+     2450000,
+     '../images/침대.png',
+     '../images/침대1.jpg',
+     'best'
+   );
+
+insert into products1 (
+   name,
+   price,
+   image_url,
+   hover_image_url,
+   category
+) values
+   ( '아루니 범퍼침대 패밀리형 (SS+Q)',
+     3090000,
+     '../images/패밀리.png',
+     '../images/패밀리2.jpg',
+     'best'
+   );
+
+insert into products1 (
+   name,
+   price,
+   image_url,
+   hover_image_url,
+   category
+) values
+   ( '마론 유아 교구장',
+     487000,
+     '../images/교구장.png',
+     '../images/소가구교구장.jpg',
+     'storage'
+   );
+
+insert into products1 (
+   name,
+   price,
+   image_url,
+   hover_image_url,
+   category
+) values
+   ( '마론 2in1 장난감정리함',
+     380000,
+     '../images/소가구정리함.png',
+     '../images/소가구정리함.jpg',
+     'storage'
+   );
+
+insert into products1 (
+   name,
+   price,
+   image_url,
+   hover_image_url,
+   category
+) values
+   ( '마론 유아 책상의자 세트',
+     465600,
+     '../images/책상의자세트.png',
+     '../images/소가구책상의자세트.jpg',
+     'desk_chair'
+   );
+
+insert into products1 (
+   name,
+   price,
+   image_url,
+   hover_image_url,
+   category
+) values
+   ( '마론 유아 의자',
+     150000,
+     '../images/유아의자.png',
+     '../images/소가구유아의자.jpg',
+     'desk_chair'
+   );
+
+insert into products1 (
+   name,
+   price,
+   image_url,
+   hover_image_url,
+   category
+) values
+   ( '마론 유아 책상',
+     330000,
+     '../images/유아책상.png',
+     '../images/소가구유아책상.jpg',
+     'desk_chair'
+   );
+
+insert into products1 (
+   name,
+   price,
+   image_url,
+   hover_image_url,
+   category
+) values
+   ( '마론 어린이 책상',
+     410000,
+     '../images/어린이책상png',
+     '../images/소가구어린이책상.jpg',
+     'desk_chair'
+   );
